0b9e8b7a7f17cf5884fcc22490881d96f6a6edfd
emptycodebox <changyuege@gmail.com>
2022-02-23 14:39:22 +0800

    init

---
+++ dist/components/Footer/config.d.ts
@@ -0,0 +1,5 @@
+import { Language } from "../LangSelector/types";
+import { FooterLinkType } from "./types";
+export declare const footerLinks: FooterLinkType[];
+export declare const socials: never[];
+export declare const langs: Language[];
